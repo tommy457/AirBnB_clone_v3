cd4f644dfdeb6232ad44e5436c611b5b52afb5e2
tommy457 <boukhalfaml1011@gmail.com>
2024-01-05 12:23:56 -0800

    endpoint stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 Index module
 """
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """returns a JSON: status"""
     return {"status": "OK"}
+
+
+@app_views.route("/stats")
+def stats():
+    """retrieves the number of each objects by type"""
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    res_dict = {}
+    for name, cls in classes.items():
+        res_dict[name] = storage.count(cls)
+    return res_dict
